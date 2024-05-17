0eefe51db581552a2ef317849c87ba73eae33f27
João Gabriel Simon Batista <ccojoao@gmail.com>
2024-05-17 19:26:33 -0300

    subindo projeto

---
+++ BaixarAudioYt/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace YoutubeAudioDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVideoUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.buttonVideo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtVideoUrl
+            // 
+            this.txtVideoUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVideoUrl.Location = new System.Drawing.Point(97, 120);
+            this.txtVideoUrl.Name = "txtVideoUrl";
+            this.txtVideoUrl.Size = new System.Drawing.Size(851, 34);
+            this.txtVideoUrl.TabIndex = 0;
+            this.txtVideoUrl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.Location = new System.Drawing.Point(243, 204);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(272, 38);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download do Áudio!";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(857, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cole o link do vídeo do Youtube abaixo e clique em download!";
+            // 
+            // status
+            // 
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(91, 266);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(857, 32);
+            this.status.TabIndex = 3;
+            this.status.Text = "Status";
+            this.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonVideo
+            // 
+            this.buttonVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonVideo.Location = new System.Drawing.Point(538, 204);
+            this.buttonVideo.Name = "buttonVideo";
+            this.buttonVideo.Size = new System.Drawing.Size(272, 38);
+            this.buttonVideo.TabIndex = 4;
+            this.buttonVideo.Text = "Download do Vídeo!";
+            this.buttonVideo.UseVisualStyleBackColor = true;
+            this.buttonVideo.Click += new System.EventHandler(this.buttonVideo_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 334);
+            this.Controls.Add(this.buttonVideo);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtVideoUrl);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVideoUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Button buttonVideo;
+    }
+}
+
---
+++ BaixarAudioYt/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using YoutubeExplode;
+
+namespace YoutubeAudioDownloader
+{
+    public partial class MainForm : Form
+    {
+        private readonly YoutubeClient _youtubeClient;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _youtubeClient = new YoutubeClient();
+            status.Text = "";
+        }
+        private async void btnDownload_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var videoUrl = txtVideoUrl.Text.Trim();
+
+                if (string.IsNullOrWhiteSpace(videoUrl))
+                {
+                    MessageBox.Show("Por favor, insira a URL do vídeo.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                status.Text = "Convertendo áudio e baixando... Aguarde!";
+
+                var video = await _youtubeClient.Videos.GetAsync(videoUrl);
+                var streamInfo = await _youtubeClient.Videos.Streams.GetManifestAsync(video.Id);
+                var audioStreamInfo = streamInfo.GetAudioStreams().OrderByDescending(s => s.Bitrate).FirstOrDefault();
+
+                if (audioStreamInfo == null)
+                {
+                    MessageBox.Show("Não foi possível encontrar um stream de áudio para este vídeo.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                var saveFileDialog = new SaveFileDialog
+                {
+                    FileName = $"{video.Title}.mp3",
+                    Filter = "Arquivos MP3|*.mp3",
+                    Title = "Salvar Áudio"
+                };
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var outputPath = saveFileDialog.FileName;
+                    await _youtubeClient.Videos.Streams.DownloadAsync(audioStreamInfo, outputPath);
+                    status.Text = "";
+                    MessageBox.Show($"Áudio baixado com sucesso como: {outputPath}", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void buttonVideo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var videoUrl = txtVideoUrl.Text.Trim();
+
+                if (string.IsNullOrWhiteSpace(videoUrl))
+                {
+                    MessageBox.Show("Por favor, insira a URL do vídeo.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                var video = await _youtubeClient.Videos.GetAsync(videoUrl);
+                var streamInfo = await _youtubeClient.Videos.Streams.GetManifestAsync(video.Id);
+                var videoStreamInfo = streamInfo.GetVideoStreams().OrderByDescending(s => s.VideoQuality).FirstOrDefault();
+
+                if (videoStreamInfo == null)
+                {
+                    MessageBox.Show("Não foi possível encontrar um stream de vídeo para este vídeo.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                var saveFileDialog = new SaveFileDialog
+                {
+                    FileName = $"{video.Title}.{videoStreamInfo.Container.Name}",
+                    Filter = $"{videoStreamInfo.Container.Name.ToUpper()} Files|*.{videoStreamInfo.Container.Name}",
+                    Title = "Salvar Vídeo"
+                };
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var outputPath = saveFileDialog.FileName;
+                    await _youtubeClient.Videos.Streams.DownloadAsync(videoStreamInfo, outputPath);
+                    MessageBox.Show($"Vídeo baixado com sucesso como: {outputPath}", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
